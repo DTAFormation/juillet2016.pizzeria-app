8fb8b9363e6b82334752f681eef2d0706b366bef
SMOHAMADY <smy-6@hotmail.com>
2016-10-14 17:33:30 +0200

    #19 lister pizza RAF filtres

--- pizzeria-website/source/pizzas/pizzas.controller.js
+++ pizzeria-website/source/pizzas/pizzas.controller.js
@@ -1,10 +1,16 @@
 (function () {
     'use strict';
-    var controller = function(pizzService, $log) {
+    var controller = function(pizzService, $log, constantImg) {
+
+        var constImg = constantImg.apiUrlImg ;
+        console.log(constImg);
+
         var vm = this;
         pizzService.getAllPizzas().then(function(result) {
             vm.allPizzas = result.data;
-        });
+        }); 
+
+        
     };
 
     module.exports = controller;
